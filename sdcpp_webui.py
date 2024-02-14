6d6dd2728f0b39eb69dd434dc2b548db6aeabaa6
DaniAndTheWeb <57776841+DaniAndTheWeb@users.noreply.github.com>
2024-02-14 05:22:38 +0000

    Rework txt2img tab
    The tab is now built using Gradio blocks.

--- sdcpp_webui.py
+++ sdcpp_webui.py
@@ -38,7 +38,7 @@ def get_next_txt2img():
     next_number = highest_number + 1
     return f"{next_number}.png"
 
-def txt2img(model, vae, ppromt, nprompt, sampling, steps, width, height, cfg, seed, threads, output):
+def txt2img(model, vae, ppromt, nprompt, sampling, steps, width, height, cfg, seed, threads, rng, output):
     fmodel = f'models/Stable-Diffusion/{model}'
     if vae:    
         fvae = f'models/VAE/{vae}'
@@ -52,13 +52,14 @@ def txt2img(model, vae, ppromt, nprompt, sampling, steps, width, height, cfg, se
     fcfg = str(cfg)
     fseed = str(seed)
     fthreads = str(threads)
+    frng = f'{rng}'
 
     if output is None or '""':
         foutput = "outputs/txt2img/" + get_next_txt2img()
     else:
         foutput = f'"outputs/txt2img/{output}.png"'
 
-    command = ['./sd', '-M', 'txt2img', '-m', fmodel, '-p', fpprompt, '--sampling-method', fsampling, '--steps', fsteps, '-W', fwidth, '-H', fheight, '--cfg-scale', fcfg, '-s', fseed, '-t', fthreads, '-o', foutput]
+    command = ['./sd', '-M', 'txt2img', '-m', fmodel, '-p', fpprompt, '--sampling-method', fsampling, '--steps', fsteps, '-W', fwidth, '-H', fheight, '--cfg-scale', fcfg, '-s', fseed, '-t', fthreads, '--rng', frng, '-o', foutput]
 
     if 'fvae' in locals():
         command.extend(['--vae', fvae])
@@ -100,25 +101,38 @@ def convert(og_model, type, gguf_model):
 def greet(name):
     return "Hello " + name + "!"
 
-txt2img = gr.Interface(
-    fn=txt2img,
-    inputs=[
-        gr.Dropdown(label="Model", choices=get_models()),
-        gr.Dropdown(label="VAE", choices=get_vaes()),
-        gr.Textbox(label="Positive Prompt"),
-        gr.Textbox(label="Negative Prompt"),
-        gr.Dropdown(label="Sampling method", choices=["euler", "euler_a", "heun", "dpm2", "dpm++2s_a", "dpm++2m", "dpm++2mv2", "lcm"], value="euler_a"),
-        gr.Slider(label="Steps", minimum=1, maximum=999, value=20),
-        gr.Slider(label="Width", minimum=1, maximum=8192, value=512),
-        gr.Slider(label="Height", minimum=1, maximum=8192, value=512),
-        gr.Slider(label="CFG Scale", minimum=1, maximum=30, value=7.0),
-        gr.Number(label="Seed", minimum=-1, maximum=2**32, value=-1),
-        gr.Number(label="Threads", minimum=0, maximum=str(os.cpu_count()), value=0),
-        gr.Textbox(label="Output Name (optional)", value="")],
-    outputs="image",
-    title="Text to Image",
-    allow_flagging="never",
-)
+with gr.Blocks() as txt2img_block:
+    txt2img_title = gr.Markdown("# Text to Image"),
+    with gr.Row():
+        with gr.Column():
+            model = gr.Dropdown(label="Model", choices=get_models())
+        with gr.Column():
+            vae = gr.Dropdown(label="VAE", choices=get_vaes())
+    with gr.Row():
+        with gr.Column(scale=3):
+            pprompt = gr.Textbox(label="Positive Prompt")
+            nprompt = gr.Textbox(label="Negative Prompt")
+        with gr.Column(scale=1):
+            gen_btn = gr.Button(value="Generate")
+
+    with gr.Row():
+        with gr.Column(scale=1):
+            with gr.Row():
+                with gr.Column(scale=1):
+                    sampling = gr.Dropdown(label="Sampling method", choices=["euler", "euler_a", "heun", "dpm2", "dpm2++2s_a", "dpm++2m", "dpm++2mv2", "lcm"], value="euler_a")
+                with gr.Column(scale=1):
+                    steps = gr.Slider(label="Steps", minimum=1, maximum=999, value=20)
+            width = gr.Slider(label="Width", minimum=1, maximum=8192, value=512)
+            height = gr.Slider(label="Height", minimum=1, maximum=8192, value=512)
+            cfg = gr.Slider(label="CFG Scale", minimum=1, maximum=30, value=7.0)
+            seed = gr.Number(label="Seed", minimum=-1, maximum=2**32, value=-1)
+            with gr.Accordion(label="Extra", open=False):
+                threads = gr.Number(label="Threads", minimum=0, maximum=os.cpu_count(), value=0)
+                rng = gr.Dropdown(label="RNG", choices=["std_default", "cuda"], value="cuda")
+                output = gr.Textbox(label="Output Name (optional)", value="")
+        with gr.Column(scale=1):
+            img_final = gr.Image()
+            gen_btn.click(txt2img, inputs=[model, vae, pprompt, nprompt, sampling, steps, width, height, cfg, seed, threads, rng, output], outputs=[img_final])
 
 img2img = gr.Interface(
 fn=greet,
@@ -141,9 +155,10 @@ convert = gr.Interface(
 )
 
 sdcpp = gr.TabbedInterface(
-    [txt2img, img2img, convert],
+    [txt2img_block, img2img, convert],
     ["txt2img", "img2img", "Checkpoint Converter"],
-    title="SDCPP",
+    title="sd.cpp-webui",
+    theme=gr.themes.Soft(),
 )
 
 sdcpp.launch()
