bce8c78ba55a1b9e03136ee80093df6b67b9e73f
Daniele <57776841+daniandtheweb@users.noreply.github.com>
2026-05-22 15:10:39 +0200

    fix: fixed typo in options tab

--- modules/interfaces/common/options_tab.py
+++ modules/interfaces/common/options_tab.py
@@ -212,7 +212,7 @@ with gr.Blocks() as options_block:
             'def_vae_cpu': performance_ui['in_vae_cpu'],
             'def_clip_cpu': performance_ui['in_clip_cpu'],
             'def_flash_attn': performance_ui['in_flash_attn'],
-            'def_diffusion-fa': performance_ui['in_diffusion_fa'],
+            'def_diffusion_fa': performance_ui['in_diffusion_fa'],
             'def_diffusion_conv_direct': performance_ui['in_diffusion_conv_direct'],
             'def_vae_conv_direct': performance_ui['in_vae_conv_direct'],
             'def_force_sdxl_vae_conv_scale': performance_ui['in_force_sdxl_vae_conv_scale']
